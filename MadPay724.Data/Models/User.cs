e0fa0c62eb9531f1206570e6e819ce4162634072
Sina Layazali <sina_unforgiven3@yahoo.com>
2019-12-31 20:49:06 +0330

    init project

---
+++ MadPay724.Data/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public class User : BaseEntity<string>
+    {
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public byte[] PaswordHash { get; set; }
+        [Required]
+        public byte[] PaswordSalt { get; set; }
+
+        public string Gender { get; set; }
+        public string DataOfBirth { get; set; }
+        public string City { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public bool Status { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+        public ICollection<BankCard> BankCards { get; set; }
+
+    }
+}
